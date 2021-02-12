4d0f281b00142155f4c41d4be14d7f618a605349
Vipul Daripkar <vipuldaripkar@gmail.com>
2021-02-12 11:11:47 +0000

    added required basic components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { BlogsComponent } from './blogs/blogs.component';
+import { BlogsListComponent } from './blogs/blogs-list/blogs-list.component';
+import { BlogDetailComponent } from './blogs/blog-detail/blog-detail.component';
+import { BlogItemComponent } from './blogs/blogs-list/blog-item/blog-item.component';
+import { AddBlogComponent } from './add-blog/add-blog.component';
+import { EditBlogComponent } from './add-blog/edit-blog/edit-blog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BlogsComponent,
+    BlogsListComponent,
+    BlogDetailComponent,
+    BlogItemComponent,
+    AddBlogComponent,
+    EditBlogComponent
   ],
   imports: [
     BrowserModule,
