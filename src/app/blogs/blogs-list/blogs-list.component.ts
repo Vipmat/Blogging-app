168d783e746597515e5170c698f60113b9c32d2a
Vipul Daripkar <vipuldaripkar@gmail.com>
2021-02-12 16:58:24 +0000

    Added components templates

--- src/app/blogs/blogs-list/blogs-list.component.ts
+++ src/app/blogs/blogs-list/blogs-list.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Blog } from '../blog.model';
 
 @Component({
   selector: 'app-blogs-list',
   templateUrl: './blogs-list.component.html',
-  styleUrls: ['./blogs-list.component.css']
+  styleUrls: ['./blogs-list.component.css'],
 })
 export class BlogsListComponent implements OnInit {
+  blogs: Blog[] = [
+    new Blog(
+      'A Test Blog',
+      'This is a simple test',
+      'https://assets.vogue.com/photos/5f5fac8b7d9362f52d645560/16:9/w_1280,c_limit/social-holding.jpg'
+    ),
+    new Blog(
+      'A Test Blog',
+      'This is a simple test',
+      'https://assets.vogue.com/photos/5f5fac8b7d9362f52d645560/16:9/w_1280,c_limit/social-holding.jpg'
+    ),
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
